c8ff464c9cb340ee413d30a77c91a5c25dbca2d7
Yevgeniy Li <alert83@gmail.com>
2020-04-23 05:28:14 +0600

    up

--- index.js
+++ index.js
@@ -2,21 +2,31 @@
 const http = require('http');
 const nodemailer = require("nodemailer");
 require('dotenv').config();
+const request = require('request-promise');
 
 let timeoutId;
 
 async function main() {
 
-  http.createServer(function (req, res) {
-    const auth = req.headers['x-authorization'];
-    if (auth === process.env.AUTH) {
+  http.createServer(async function async(req, res) {
+    if (xAuthIsValid(req, res)) {
       console.log('reset');
       reset();
+      await sendState('clear');
     }
-    res.end(); //end the response
+    res.end();
   }).listen(process.env.PORT);
 
   reset();
+}
+
+function xAuthIsValid(req, res) {
+  if (req.header('x-authorization') === process.env.AUTH) {
+    return true;
+  }
+
+  res.status(401).send('Unauthorized');
+  return false;
 }
 
 function reset() {
@@ -50,9 +60,32 @@ async function onTimeOut() {
     html: "<b>watch dog raised</b>",
   });
 
+  await sendState('detected');
+
   transporter.close();
 
   return info;
+}
+
+async function sendState(value) {
+  await request({
+    method: 'POST',
+    uri: 'https://my-oauth2-server.herokuapp.com/st/command',
+    headers: {
+      'x-authorization': process.env.AUTH2
+    },
+    body: {
+      "devices": [{
+        "deviceId": "5e9f6449cbfe427e4234bb98",
+        "states": [{
+          "capability": "st.tamperAlert",
+          "attribute": "tamper",
+          "value": value,
+        }]
+      }]
+    },
+    json: true
+  });
 }
 
 module.exports = {onTimeOut};
